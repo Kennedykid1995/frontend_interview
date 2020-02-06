960f260d54fbc1aee853d8bdd4c501357c5667ce
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-06 15:10:35 -0600

    setting up pages

--- frontint/src/App.js
+++ frontint/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home"; 
+import Post from './pages/Post'; 
+import Login from "./pages/Login"; 
+import Register from "./pages/Register"; 
+import Landing from "./pages/Landing"; 
+import axios from 'axios'; 
+import {Route, Link, NavLink} from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
-     starting a new
+    <Route 
+     exact path="/"
+     render={props => <Home {...props} />}
+    />
+    <Route 
+     exact path="/post"
+     render={props => <Post {...props} />}
+    />
+    <Route 
+     exact path="/landing"
+     render={props => <Landing {...props} />}
+    />
+    <Route 
+     exact path="/login"
+     render={props => <Login {...props} />}
+    />
+    <Route 
+     exact path="/register"
+     render={props => <Register {...props} />}
+    />
     </div>
   );
 }
