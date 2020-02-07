5ea044eb4c28935805ac2cd44c815fe3473c8454
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-07 10:48:18 -0600

    structuring the ui

--- frontint/src/components/Card.js
+++ frontint/src/components/Card.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles(theme  => ({
+const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
     },
     paper: {
         padding: theme.spacing(2),
-        textAlign:'center',
+        textAlign: 'center',
         color: theme.palette.text.secondary
     },
 }));
 
 
-export default function Card(){
-    const classes = useStyles(); 
-        return (
-            <div className={classes.root}>
-              <Paper className={classes.paper}>xs=12</Paper>
-            </div>
-        )
-    }
+export default function Card() {
+    const classes = useStyles();
+    return (
+        <Paper className={classes.paper}></Paper>
+    )
+}
--- frontint/src/pages/Home.js
+++ frontint/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import Card from '../components/Card'; 
+import Card from '../components/Card';
+import Grid from '@material-ui/core/Grid';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Card />
+                home
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card />
+                    </Grid>
+                </Grid>
             </div>
         )
     }
