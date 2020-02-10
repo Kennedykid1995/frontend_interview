b2db44134642440bb97cc9592dacae98ca221f20
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-09 20:35:12 -0600

    got margins to work

--- frontint/src/components/Card.js
+++ frontint/src/components/Card.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
     root: {
-        flexGrow: 1,
+        margin: theme.spacing(1),
+        spacing: [2, 4, 6, 8, 10]
     },
     paper: {
         padding: theme.spacing(2),
-        boxShadow: 'none', 
-        backgroundColor:'#f200ff',
+        boxShadow: 'none',
+        backgroundColor: '#f200ff',
         textAlign: 'center',
-        color: 'black'
-
+        color: 'black',
+        display: "flex",
+        flexDirection: "column",
     },
-    button:{
+    button: {
         variant: "contained",
         backgroundColor: "white",
-        boxShadow: "none"
+        boxShadow: "none",
+        margin: 20
+    },
+    titleBox: {
+        id: "standard-basic",
+        label: "Standard",
+        margin: 20
+    },
+    textFeild: {
+        id: "outlined-multiline-static",
+        label: "Multiline",
+        rows: "4",
+        variant: "filled",
+        margin: 20
     }
 }));
 
@@ -26,8 +42,13 @@ const useStyles = makeStyles(theme => ({
 export default function Card() {
     const classes = useStyles();
     return (
-        <Paper className={classes.paper}>
-            <Button className={classes.button}>Add Note</Button>
-        </Paper>
+        <form className={classes.root}>
+            <Paper className={classes.paper}>
+                <TextField className={classes.titleBox} />
+                <TextField multiline />
+                <Button className={classes.button}>Add Note</Button>
+            </Paper>
+        </form>
+
     )
 }
