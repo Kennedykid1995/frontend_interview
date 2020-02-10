bcf5c7277f63638432e850e554eb32e072ec18ec
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-10 10:20:08 -0600

    working on the UI

--- frontint/src/components/Note.js
+++ frontint/src/components/Note.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-
 const useStyles = makeStyles(theme => ({
     root: {
         margin: theme.spacing(1),
@@ -12,7 +10,7 @@ const useStyles = makeStyles(theme => ({
     paper: {
         padding: theme.spacing(2),
         boxShadow: 'none',
-        backgroundColor: '#f200ff',
+        backgroundColor: '#00ffbb',
         textAlign: 'center',
         color: 'black',
         display: "flex",
@@ -24,32 +22,15 @@ const useStyles = makeStyles(theme => ({
         boxShadow: "none",
         margin: 20
     },
-    titleBox: {
-        id: "standard-basic",
-        label: "Standard",
-        margin: 20,
-        
-    },
-    textFeild: {
-        id: "outlined-multiline-static",
-        label: "Multiline",
-        rows: "4",
-        variant: "filled",
-        margin: 20
-    }
 }));
 
-
-export default function Card() {
+export default function Note() {
     const classes = useStyles();
     return (
-        <form className={classes.root}>
+        <div className={classes.root}>
             <Paper className={classes.paper}>
-                <TextField className={classes.titleBox} />
-                <TextField multiline />
-                <Button className={classes.button}>Add Note</Button>
+                <Button className={classes.button}>View Note</Button>
             </Paper>
-        </form>
-
+        </div>
     )
 }
--- frontint/src/pages/Home.js
+++ frontint/src/pages/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Card from '../components/Card';
 import Grid from '@material-ui/core/Grid';
-
-export default class Home extends Component {
+import Note from '../components/Note'
+; export default class Home extends Component {
     render() {
         return (
             <div>
                 <Grid container spacing={3}>
                     <Grid item xs={6}>
                         <Card />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Note />
                     </Grid>
                 </Grid>
             </div>
