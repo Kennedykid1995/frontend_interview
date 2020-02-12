1af30269af54383d7bf9b892b783c406bacaf503
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-02-12 12:28:07 -0600

    working on view note

---
+++ frontint/src/components/ViewNote.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import { Link, NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const url = "http://localhost:4000/notes"
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: theme.spacing(1),
+        spacing: [2, 4, 6, 8, 10]
+    },
+    holder: {
+        height: 600,
+        overflow: "scroll",
+    },
+    paper: {
+        padding: theme.spacing(2),
+        boxShadow: 'none',
+        backgroundColor: '#00ffbb',
+        textAlign: 'center',
+        color: 'black',
+        display: "flex",
+        flexDirection: "column",
+    },
+    button: {
+        variant: "contained",
+        backgroundColor: "white",
+        boxShadow: "none",
+        padding: 10,
+        margin: 10
+    },
+    noteArea: {
+        textAlign: "left",
+    },
+    link: {
+        textDecoration: 'none'
+    }
+}))
+
+export default function ViewNote(){
+        return (
+            <div>
+                
+            </div>
+        )
+}
